1a49119f7538c42b51ec35f80dc1052f7bbb1e7a
CauT <donglingyongadls@gmail.com>
2015-11-07 19:35:38 +0800

    achieve css and redirect to rendered blog page

--- routes/upload.js
+++ routes/upload.js
@@ -2,13 +2,25 @@ var express = require('express');
 var router = express.Router();
 var url = require('url');
 var mongoose = require('mongoose');
+var marked = require('marked');
 
 var Page = require('../models/page').Page;
+
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: true,
+  smartLists: true,
+  smartypants: false
+});
 
 mongoose.connect('mongodb://localhost:27017/test');
 
 router.get('/page', function(req, res, next) {
-    res.render('upload_page');
+    res.render('upload_page', {css_path: '/stylesheets/upload.css'});
 });
 
 router.post('/page', function(req, res, next) {
@@ -18,7 +30,7 @@ router.post('/page', function(req, res, next) {
         created_at: date,
         updated_at: date
     });
-    newPage.content = req.body.page;
+    // newPage.content = req.body.page;
     newPage.title = req.body.title;
 
     // check uniqueness of tags, if true, put it into database
@@ -35,10 +47,19 @@ router.post('/page', function(req, res, next) {
         }
     }
 
-    newPage.save(function(err, room) {
+    marked(req.body.page, function(err, content) {
         if (err) throw err;
+        newPage.content = content;
+        newPage.save(function(err, room) {
+            if (err) throw err;
 
-        console.log(room);
+            // console.log(room);
+            // res.redirect('your/404/path.html');
+            res.render('page', {
+                css_path: '/stylesheets/greyshade.css',
+                marked_page: room.content
+            });
+        });
     });
 });
 
